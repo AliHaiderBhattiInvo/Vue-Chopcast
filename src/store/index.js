4b1311586b60a7a2bc99950fb840078fe37e121d
Ali Haider Bhatti <ali.haider@invozone.com>
2023-02-08 01:32:09 +0500

    First commit

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,26 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    data: {},
+    selectedLanguage: { label: "English", value: "en" },
+  },
+  getters: {
+    getData: (state) => state.data,
+    getSelectedLanguage: (state) => state.selectedLanguage,
+  },
+  mutations: {
+    setData(state, payload) {
+      state.data = payload;
+    },
+    setSelectedLanguage(state, payload) {
+      state.selectedLanguage = payload;
+    },
+  },
+  actions: {
+    saveData({ commit }, payload) {
+      commit("setData", payload);
+    },
+  },
   modules: {},
 });
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "enn",
+      localeDir: "locales",
+      enableInSFC: true,
+      includeLocales: false,
+      enableBridge: true,
+    },
+  },
 });
